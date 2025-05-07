cdc97336d38238eaa3770e5381b3e0ffc256f33d
notKshen <shenkobe.111@gmail.com>
2025-05-06 18:29:05 -0700

    Initial Commit

---
+++ Splitly/SplitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitlyApp.swift
+//  Splitly
+//
+//  Created by Kobe Shen on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct SplitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
